dc28398b2d8d8ef33d9404fee5c5a7a95b268616
andrea savignano <112469730+andreasavignano02@users.noreply.github.com>
2023-01-11 10:44:17 +0100

    add db

---
+++ Agenzia/Agenzia/Models/Spedizioni.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Agenzia.Models
+{
+    public class Spedizioni
+    {
+        public int ID { get; set; }
+        public string NomeArticolo { get; set; }
+        public int NumeroIdentificativo { get; set; }
+        public DateTime DataDiSpedizione { get; set; }
+        public double Peso { get; set; }
+        public string CittaDestinatario { get; set; }
+        public string Indirizzo { get; set; }
+        public string NomeDestinatario { get; set; }
+        public decimal CostoArticolo { get; set; }
+        public DateTime DataDiArrivo { get; set; } 
+    }
+}
---
+++ Agenzia/Agenzia/Models/Utente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Agenzia.Models
+{
+    public class Utente
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CodiceFiscale { get; set; }
+        public string PartitaIVA { get; set; }
+    }
+}
